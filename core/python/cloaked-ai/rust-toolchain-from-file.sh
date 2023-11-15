d6ad17abbe3a84444e1223e54d834c92dfee4b2a
Murph Murphy <murph@clurictec.com>
2023-11-14 18:30:06 -0700

    Closer to done script

--- core/python/cloaked-ai/rust-toolchain-from-file.sh
+++ core/python/cloaked-ai/rust-toolchain-from-file.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 # Used by readthedocs to get the right version of rust installed. Based on our github action that does the same.
-pip install yq tomlq
+pip install yq tomlq jq
 for F in rust-toolchain.toml rust-toolchain ; do
   if [ -f "$F" ] ; then
     TOML_FILE="$F"
@@ -12,15 +12,8 @@ if [ -z "$TOML_FILE" ]; then
   exit 0
 fi
 TOML_TOOLCHAIN=$(tomlq -r '.toolchain.channel | select(. != null)' "$TOML_FILE")
-if [ -n "$TOML_TOOLCHAIN" ]; then
-  echo "toml-toolchain=$TOML_TOOLCHAIN" >> "$GITHUB_OUTPUT"
-fi
 TOML_TARGETS=$(tomlq -r '.toolchain.targets | select(. != null) | @csv' "$TOML_FILE")
-if [ -n "$TOML_TARGETS" ]; then
-  echo "toml-targets=$TOML_TARGETS" >> "$GITHUB_OUTPUT"
-fi
 TOML_COMPONENTS=$(tomlq -r '.toolchain.components | select(. != null) | @csv' "$TOML_FILE")
-if [ -n "$TOML_COMPONENTS" ]; then
-  echo "toml-components=$TOML_COMPONENTS" >> "$GITHUB_OUTPUT"
-fi
-
+rustup toolchain install "$TOML_TOOLCHAIN""$TOML_TARGETS""$TOML_COMPONENTS" --profile minimal --no-self-update
+rustup default "$TOML_TOOLCHAIN"
+rustup override set "$TOML_TOOLCHAIN"
