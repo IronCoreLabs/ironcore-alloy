d7b81f9ed15e7344590cd80ca01a32473cd58148
Leeroy Travis <ops@ironcorelabs.com>
2023-11-16 02:07:48 +0000

    Set release version 0.9.0 [skip ci]

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-version = "0.9.0-SNAPSHOT"
+version = "0.9.0"
 group = "com.ironcorelabs"
 
 plugins {
