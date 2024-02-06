64cfbb6edab3e09a295e17623a53bf76b4792f81
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2024-02-06 10:46:38 -0800

    Add deterministic filter to Weaviate example

--- example-integrations/weaviate/weaviate-semantic-search.py
+++ example-integrations/weaviate/weaviate-semantic-search.py
@@ -1,11 +1,3 @@
-# PREREQUISITE:
-#   Start up Weaviate locally as described on the [Docker Compose](https://weaviate.io/developers/weaviate/installation/docker-compose)
-#   installation guide.
-# DEPENDENCIES:
-#   `pip install -U weaviate_client sentence_transformers ironcore-alloy`
-# REFERENCES:
-#   This is based on the Weaviate [Bring your own vectors](https://weaviate.io/developers/weaviate/starter-guides/custom-vectors) starter guide.
-
 import asyncio
 import weaviate
 import ironcore_alloy as alloy
@@ -14,6 +6,23 @@ from sentence_transformers import SentenceTransformer
 import json
 import weaviate.classes as wvc
 from urllib.request import urlopen
+
+
+async def display_results(
+    response, alloy_client: alloy.Standalone, metadata: alloy.AlloyMetadata
+):
+    # Decrypt the results and display them
+    for result in response.objects:
+        recreated_question = base64.b64decode(result.properties["question"])  # type: ignore
+        decrypted_question = await alloy_client.standard_attached().decrypt(
+            recreated_question, metadata
+        )
+        recreated_round = alloy.EncryptedField(base64.b64decode(result.properties["round"]), secret_path, derivation_path)  # type: ignore
+        # We have to deterministically decrypt the round because it was deterministically encrypted earlier
+        decrypted_round = await alloy_client.deterministic().decrypt(
+            recreated_round, metadata
+        )
+        print(f"{round(result.metadata.certainty, 2)}: {decrypted_question.decode()} ({decrypted_round.plaintext_field.decode()})")  # type: ignore
 
 
 async def main():
@@ -50,34 +59,46 @@ async def main():
         )
     }
     standard_secrets = alloy.StandardSecrets(1, [standalone_secret])
-    deterministic_secrets = {}
+    deterministic_secrets = {"jeopardy": alloy.RotatableSecret(standalone_secret, None)}
     config = alloy.StandaloneConfiguration(
         standard_secrets, deterministic_secrets, vector_secrets
     )
     alloy_client = alloy.Standalone(config)
 
-    tenant_id = alloy.AlloyMetadata.new_simple("tenant-one")
+    metadata = alloy.AlloyMetadata.new_simple("tenant-one")
+    secret_path = "jeopardy"
+    derivation_path = "sentence"
 
     print("Transforming questions to vectors and encrypting them...")
     question_objs = list()
     for row in data:
         question_emb = model.encode(row["Question"]).tolist()  # type: ignore
         # each index and set of vectors is encrypted with different derived keys
-        plaintext_vector = alloy.PlaintextVector(question_emb, "jeopardy", "sentence")
+        plaintext_vector = alloy.PlaintextVector(
+            question_emb, secret_path, derivation_path
+        )
         # Some questions contain HTML tags that muddle the results, so we'll skip inserting those ones
         if "<" in row["Question"]:
             continue
-        # Encrypt both the vector and the question itself
-        (encrypted_vector, encrypted_metadata) = await asyncio.gather(
-            alloy_client.vector().encrypt(plaintext_vector, tenant_id),
+        # Encrypt the vector, the question itself, and the round
+        # The round is deterministically encrypted so that we can filter on it later
+        (encrypted_vector, encrypted_question, encrypted_round) = await asyncio.gather(
+            alloy_client.vector().encrypt(plaintext_vector, metadata),
             alloy_client.standard_attached().encrypt(
-                bytes(row["Question"], "utf-8"), tenant_id
+                bytes(row["Question"], "utf-8"), metadata
+            ),
+            alloy_client.deterministic().encrypt(
+                alloy.PlaintextField(
+                    bytes(row["Round"], "utf-8"), secret_path, derivation_path
+                ),
+                metadata,
             ),
         )
         question_objs.append(
             wvc.data.DataObject(
                 properties={
-                    "question": base64.b64encode(encrypted_metadata).decode(),
+                    "question": base64.b64encode(encrypted_question).decode(),
+                    "round": base64.b64encode(encrypted_round.encrypted_field).decode(),
                 },
                 vector=encrypted_vector.encrypted_vector,
             )
@@ -88,18 +109,18 @@ async def main():
     questions.data.insert_many(question_objs)
 
     query = "biology"
-    print(f"Querying database with input: '{query}'")
+    print(f"\nQuerying database with input: '{query}'")
 
     # Create the query embedding
     query_emb = model.encode(query).tolist()  # type: ignore
-    plaintext_query = alloy.PlaintextVector(query_emb, "jeopardy", "sentence")
+    plaintext_query = alloy.PlaintextVector(query_emb, secret_path, derivation_path)
     # `generate_query_vectors` returns a list because the secret involved may be in rotation. In that case you should
     # search for both resulting vectors. Weaviate currently doesn't support searching over multiple vectors. A workaround is
     # searching for each separately and combining them in the client.
     # Because our secret isn't in rotation, we can just use the first entry in the list.
     query_vector = (
         await alloy_client.vector().generate_query_vectors(
-            {"vec_1": plaintext_query}, tenant_id
+            {"vec_1": plaintext_query}, metadata
         )
     )["vec_1"][0].encrypted_vector
 
@@ -108,14 +129,28 @@ async def main():
         near_vector=query_vector,
         limit=5,
         return_metadata=wvc.query.MetadataQuery(certainty=True),
-        return_properties=["question"],
+        return_properties=["question", "round"],
     )
+    await display_results(response, alloy_client, metadata)
 
-    # Decrypt the results and display them
-    for result in response.objects:
-        recreated = base64.b64decode(result.properties["question"])  # type: ignore
-        decrypted = await alloy_client.standard_attached().decrypt(recreated, tenant_id)
-        print(f"{round(result.metadata.certainty, 2)}: {decrypted.decode()}")  # type: ignore
+    print("\nQuerying again, this time only for Double Jeopardy questions.")
+    # Now we'll query Weaviate a second time, only returning answers from the Double Jeopardy round
+    round_filter = await alloy_client.deterministic().encrypt(
+        alloy.PlaintextField(
+            bytes("Double Jeopardy!", "utf-8"), secret_path, derivation_path
+        ),
+        metadata,
+    )
+    response = questions.query.near_vector(
+        near_vector=query_vector,
+        limit=5,
+        return_metadata=wvc.query.MetadataQuery(certainty=True),
+        return_properties=["question", "round"],
+        filters=wvc.query.Filter.by_property("round").equal(
+            base64.b64encode(round_filter.encrypted_field).decode()
+        ),
+    )
+    await display_results(response, alloy_client, metadata)
 
     client.close()
 
