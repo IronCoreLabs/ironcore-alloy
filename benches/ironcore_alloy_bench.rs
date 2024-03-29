a0fc1356dfdd8dbb2d9946b3446d81b9d228e477
Colt Frederickson <coltfred@gmail.com>
2024-03-29 10:32:15 -0600

    Add benches with TSP

--- benches/ironcore_alloy_bench.rs
+++ benches/ironcore_alloy_bench.rs
@@ -1,13 +1,15 @@
+use std::env;
 use std::time::Duration;
 
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use ironcore_alloy::saas_shield::config::SaasShieldConfiguration;
 use ironcore_alloy::standalone::config::{
     RotatableSecret, StandaloneConfiguration, StandaloneSecret, StandardSecrets, VectorSecret,
 };
-use ironcore_alloy::standard::StandardDocumentOps;
+use ironcore_alloy::standard::{EncryptedDocument, PlaintextDocument, StandardDocumentOps};
 use ironcore_alloy::vector::{PlaintextVector, VectorOps};
-use ironcore_alloy::DerivationPath;
 use ironcore_alloy::{AlloyMetadata, Secret, SecretPath, Standalone, TenantId};
+use ironcore_alloy::{DerivationPath, SaasShield};
 use itertools::Itertools;
 use rand::rngs::ThreadRng;
 use rand::{Rng, RngCore};
@@ -36,7 +38,6 @@ use tokio::runtime::Runtime;
 //     //     )
 //     // });
 // }
-
 fn benches(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
     let mut key_bytes = [0u8; 64];
@@ -103,14 +104,14 @@ fn benches(c: &mut Criterion) {
     });
     c.bench_function(format!("encrypt/decrypt roundtrip medium").as_str(), |b| {
         b.to_async(Runtime::new().unwrap()).iter_batched(
-            || random_word(rng.clone(), 10 * 1000),
+            || random_word(rng.clone(), 10_000),
             roundtrip,
             BatchSize::SmallInput,
         )
     });
     c.bench_function(format!("encrypt/decrypt roundtrip large").as_str(), |b| {
         b.to_async(Runtime::new().unwrap()).iter_batched(
-            || random_word(rng.clone(), 10 * 10000),
+            || random_word(rng.clone(), 100_000),
             roundtrip,
             BatchSize::SmallInput,
         )
@@ -127,9 +128,82 @@ fn benches(c: &mut Criterion) {
     // }
 }
 
+fn tsp_benches(c: &mut Criterion) {
+    let rng = rand::thread_rng();
+    let tsp_uri = env::var("TSP_ADDRESS").unwrap_or("http://localhost".to_string());
+    let tsp_port = env::var("TSP_PORT").unwrap_or("32804".to_string());
+    let tenant_id = env::var("TENANT_ID").unwrap_or("tenant-gcp".to_string());
+    let api_key = env::var("API_KEY").unwrap_or("0WUaXesNgbTAuLwn".to_string());
+
+    let config =
+        SaasShieldConfiguration::new(format!("{tsp_uri}:{tsp_port}"), api_key, true, None).unwrap();
+    let sdk = SaasShield::new(&config);
+    let metadata = AlloyMetadata::new_simple(TenantId(tenant_id));
+
+    let encrypt = |plaintext: PlaintextDocument| async {
+        sdk.standard().encrypt(plaintext, &metadata).await.unwrap()
+    };
+
+    let decrypt = |encrypted: EncryptedDocument| async {
+        sdk.standard().decrypt(encrypted, &metadata).await.unwrap()
+    };
+
+    let roundtrip = |plaintext: PlaintextDocument| async {
+        let encrypted = encrypt(plaintext).await;
+        decrypt(encrypted).await
+    };
+
+    fn random_word(rng: ThreadRng, length: usize) -> Vec<u8> {
+        rng.sample_iter(&Alphanumeric)
+            .take(length)
+            .map(char::from)
+            .collect::<String>()
+            .into_bytes()
+    }
+
+    c.bench_function(
+        format!("TSP encrypt/decrypt roundtrip fixed document").as_str(),
+        |b| {
+            b.to_async(Runtime::new().unwrap()).iter_batched(
+                || {
+                    [(
+                        "doc1".to_string(),
+                        "Encrypt these bytes!".as_bytes().to_vec(),
+                    )]
+                    .into()
+                },
+                roundtrip,
+                BatchSize::SmallInput,
+            )
+        },
+    );
+
+    c.bench_function(format!("TSP encrypt 10KB").as_str(), |b| {
+        b.to_async(Runtime::new().unwrap()).iter_batched(
+            || [("doc1".to_string(), random_word(rng.clone(), 10_000))].into(),
+            encrypt,
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function(format!("TSP encrypt 100KB").as_str(), |b| {
+        b.to_async(Runtime::new().unwrap()).iter_batched(
+            || [("doc1".to_string(), random_word(rng.clone(), 100_000))].into(),
+            encrypt,
+            BatchSize::SmallInput,
+        )
+    });
+}
+
 criterion_group! {
-    name = benchmarks;
+    name = standalone_benchmarks;
     config = Criterion::default().measurement_time(Duration::from_secs(10));
     targets = benches
 }
-criterion_main!(benchmarks);
+
+criterion_group! {
+    name = tsp_benchmarks;
+    config = Criterion::default().measurement_time(Duration::from_secs(10));
+    targets = tsp_benches
+}
+criterion_main!(standalone_benchmarks, tsp_benchmarks);
