9a0efd67ec22e0a7b5d67ce93d294a5d49d72a71
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2024-04-04 14:43:31 -0700

    Fix Kotlin version number (#104)

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.allopen.gradle.*
 import org.jetbrains.kotlin.gradle.tasks.*
 import kotlinx.benchmark.gradle.*
 
-version = "0.10.4-SNAPSHOT"
+version = "0.11.0-SNAPSHOT"
 
 group = "com.ironcorelabs"
 
