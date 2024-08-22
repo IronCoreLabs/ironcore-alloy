6185eac5d0b78307a3265648f137f5cf3a7068f5
Murph Murphy <murph@clurictec.com>
2024-08-22 17:17:31 -0600

    Update python examples

--- examples/python/saas-shield/vector-roundtrip.py
+++ examples/python/saas-shield/vector-roundtrip.py
@@ -4,16 +4,22 @@ import asyncio
 
 
 async def main():
-    tenant_id = os.environ.get('TENANT_ID', 'tenant-gcp')
-    api_key = os.environ.get('API_KEY', '0WUaXesNgbTAuLwn')
+    tenant_id = os.environ.get("TENANT_ID", "tenant-gcp")
+    api_key = os.environ.get("API_KEY", "0WUaXesNgbTAuLwn")
     tsp_uri = "http://localhost:32804"
     approximation_factor = 7.2
-    config = alloy.SaasShieldConfiguration(tsp_uri, api_key, False, approximation_factor)
+    config = alloy.SaasShieldConfiguration(
+        tsp_uri, api_key, False, approximation_factor
+    )
     sdk = alloy.SaasShield(config)
 
     data = [1.2, -1.23, 3.24, 2.37]
     print("Plaintext vector: ", data)
-    plaintext = alloy.PlaintextVector(data, "contacts", "conversation-sentiment")
+    plaintext = alloy.PlaintextVector(
+        plaintext_vector=data,
+        secret_path="contacts",
+        derivation_path="conversation-sentiment",
+    )
     metadata = alloy.AlloyMetadata.new_simple(tenant_id)
     encrypted = await sdk.vector().encrypt(plaintext, metadata)
     print("Encrypted vector: ", encrypted.encrypted_vector)
@@ -24,6 +30,6 @@ async def main():
     print("Note that the encryption/decryption is lossy due to floating point math.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
--- examples/python/standalone/vector-roundtrip.py
+++ examples/python/standalone/vector-roundtrip.py
@@ -4,25 +4,32 @@ import asyncio
 
 
 async def main():
-    tenant_id = os.environ.get('TENANT_ID', 'tenant-gcp')
+    tenant_id = os.environ.get("TENANT_ID", "tenant-gcp")
     # Note: in practice this must be 32 cryptographically-secure bytes
     key_bytes = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     approximation_factor = 7.2
     vector_secrets = {
-        "contacts":
-            alloy.VectorSecret(
-                approximation_factor,
-                alloy.RotatableSecret(alloy.StandaloneSecret(1, alloy.Secret(key_bytes)), None),
-            )
+        "contacts": alloy.VectorSecret(
+            approximation_factor,
+            alloy.RotatableSecret(
+                alloy.StandaloneSecret(1, alloy.Secret(key_bytes)), None
+            ),
+        )
     }
     standard_secrets = alloy.StandardSecrets(None, [])
     deterministic_secrets = {}
-    config = alloy.StandaloneConfiguration(standard_secrets, deterministic_secrets, vector_secrets)
+    config = alloy.StandaloneConfiguration(
+        standard_secrets, deterministic_secrets, vector_secrets
+    )
     sdk = alloy.Standalone(config)
 
     data = [1.2, -1.23, 3.24, 2.37]
     print("Plaintext vector: ", data)
-    plaintext = alloy.PlaintextVector(data, "contacts", "conversation-sentiment")
+    plaintext = alloy.PlaintextVector(
+        plaintext_vector=data,
+        secret_path="contacts",
+        derivation_path="conversation-sentiment",
+    )
     metadata = alloy.AlloyMetadata.new_simple(tenant_id)
     encrypted = await sdk.vector().encrypt(plaintext, metadata)
     print("Encrypted vector: ", encrypted.encrypted_vector)
@@ -33,6 +40,6 @@ async def main():
     print("Note that the encryption/decryption is lossy due to floating point math.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
