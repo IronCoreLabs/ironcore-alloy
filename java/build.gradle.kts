e53c1289a16e79ee38adf01c9504dc50453e3b82
Leeroy Travis <ops@ironcorelabs.com>
2025-06-06 19:32:13 +0000

    Bump to next development version 0.12.1-pre [skip ci]

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-version = "0.12.0"
+version = "0.12.1-SNAPSHOT"
 
 group = "com.ironcorelabs"
 
